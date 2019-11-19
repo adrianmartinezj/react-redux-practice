ee11aa96706b7e231e290b46a2215fc926b68db7
Adrian Martinez <adrianjustinmartinez@gmail.com>
2019-11-19 10:43:55 -0800

    Form logic

--- src/routes/Landing.js
+++ src/routes/Landing.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, {
+    useState,
+} from 'react'
 import classes from '../css/Landing.module.css'
 
 const Landing = (props) => {
+    const [formSubmission, setFormSubmission] = useState({
+        firstName: '',
+        lastName: '',
+        userName: '',
+        email: '',
+        password: '',
+    })
+
+    const [confirmPassword, setConfirmPasssword] = useState('')
+
     const handleSubmit = (e) => {
-        console.log('e.target.value')
+        console.log(e)
+        e.preventDefault();
+    }
+
+    const handleClear = (e) => {
+        setFormSubmission({
+            firstName: '',
+            lastName: '',
+            userName: '',
+            email: '',
+            password: '',
+        })
+        e.preventDefault();
+    }
+
+    const modifyFormSubmission = (e, type) => {
+        let newFormSubmission = {...formSubmission}
+        newFormSubmission[type] = e.target.value
+        setFormSubmission(newFormSubmission)
+    }
+
+    const handlePasswordCheck = (e) => {
+        setConfirmPasssword(e.target.value)
     }
 
     return (
@@ -11,17 +45,22 @@ const Landing = (props) => {
             Welcome
             <form className={classes.formContainer} onSubmit={handleSubmit}>
                 <label>First Name</label>
-                <input />
+                <input value={formSubmission.firstName} onChange={(e) => {modifyFormSubmission(e, 'firstName')}} />
                 <label>Last Name</label>
-                <input />
+                <input value={formSubmission.lastName} onChange={(e) => {modifyFormSubmission(e, 'lastName')}} />
                 <label>Username</label>
-                <input />
+                <input value={formSubmission.userName} onChange={(e) => {modifyFormSubmission(e, 'userName')}} />
                 <label>E-mail</label>
-                <input />
+                <input value={formSubmission.email} onChange={(e) => {modifyFormSubmission(e, 'email')}} />
                 <label>Password</label>
-                <input type='password'/>
+                <input value={formSubmission.password} onChange={(e) => {modifyFormSubmission(e, 'password')}} type='password'/>
                 <label>Confirm Password</label>
-                <input type='password'/>
+                <input onChange={handlePasswordCheck} type='password'/>
+                {formSubmission.password.length > 0 ? confirmPassword !== formSubmission.password ? <label>Password does not match</label> : <label>Passwords match</label> : null}
+                <div>
+                    <button>Submit</button>
+                    <button onClick={handleClear}>Clear</button>
+                </div>
             </form>
         </div>
     )
