9552d64647d96dd0cb744a9a248d579836ec48d7
Adrian Martinez <adrianjustinmartinez@gmail.com>
2019-11-19 10:26:59 -0800

    Styling for landing page, setup form

--- src/routes/Landing.js
+++ src/routes/Landing.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import classes from '../css/Landing.module.css'
 
 const Landing = (props) => {
+    const handleSubmit = (e) => {
+        console.log('e.target.value')
+    }
+
     return (
-        <div>
-            Landing
+        <div className={classes.mainContainer}>
+            Welcome
+            <form className={classes.formContainer} onSubmit={handleSubmit}>
+                <label>First Name</label>
+                <input />
+                <label>Last Name</label>
+                <input />
+                <label>Username</label>
+                <input />
+                <label>E-mail</label>
+                <input />
+                <label>Password</label>
+                <input type='password'/>
+                <label>Confirm Password</label>
+                <input type='password'/>
+            </form>
         </div>
     )
 }
